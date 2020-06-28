d933299c25173fbd3a4f6e639ad4151c453f03a1
Alem Bilic <alembily@gmail.com>
2020-06-28 13:05:18 +0200

    k

--- app/Product.php
+++ app/Product.php
@@ -7,7 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    public function reviews(){
-        return $this->belongsToMany('App\Review', 'reviews','id', 'product_id');
-    }
+
 }
